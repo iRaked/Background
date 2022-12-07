f81a669c7687815771d0b28ea4546fbe4f5c71b4
Raked <75103024+iRaked@users.noreply.github.com>
2022-12-06 20:52:59 -0600

    Add files via upload

---
+++ BG Xmas/java.js
@@ -0,0 +1,88 @@
+(function () {
+
+  var COUNT = 300;
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+  var i = 0;
+  var active = false;
+
+  function onResize() {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    ctx.fillStyle = '#FFF';
+
+    var wasActive = active;
+    active = innerWidth > 600;
+
+    if (!wasActive && active)
+      requestAnimFrame(update);
+  }
+
+  var Snowflake = function () {
+    this.x = 0;
+    this.y = 0;
+    this.vy = 0;
+    this.vx = 0;
+    this.r = 0;
+
+    this.reset();
+  }
+
+  Snowflake.prototype.reset = function() {
+    this.x = Math.random() * innerWidth;
+    this.y = Math.random() * -innerHeight;
+    this.vy = 1 + Math.random() * 3;
+    this.vx = 0.5 - Math.random();
+    this.r = 1 + Math.random() * 2;
+    this.o = 0.5 + Math.random() * 0.5;
+  }
+
+  canvas.style.position = 'absolute';
+  canvas.style.left = canvas.style.top = '0';
+
+  var snowflakes = [], snowflake;
+  for (i = 0; i < COUNT; i++) {
+    snowflake = new Snowflake();
+    snowflake.reset();
+    snowflakes.push(snowflake);
+  }
+
+  function update() {
+
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    if (!active)
+      return;
+
+    for (i = 0; i < COUNT; i++) {
+      snowflake = snowflakes[i];
+      snowflake.y += snowflake.vy;
+      snowflake.x += snowflake.vx;
+
+      ctx.globalAlpha = snowflake.o;
+      ctx.beginPath();
+      ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+      ctx.closePath();
+      ctx.fill();
+
+      if (snowflake.y > innerHeight) {
+        snowflake.reset();
+      }
+    }
+
+    requestAnimFrame(update);
+  }
+
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ){
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+  onResize();
+  window.addEventListener('resize', onResize, false);
+
+})();
